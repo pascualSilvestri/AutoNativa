4d67bbebbf6a9fbe323bc086c02d372eb00c3a8a
PascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2022-05-20 10:06:12 -0300

    p

---
+++ main2.js
@@ -0,0 +1,13 @@
+addEventListener('DOMContentLoaded',()=>{
+    const btn = document.querySelector('.btn-menu');
+    if(btn){
+        btn.addEventListener('click',()=>{
+            const nav = document.querySelector('.nav-ul-respon');
+            nav.classList.toggle('mostrar'); 
+        })
+    }
+})
+
+$("document").ready(function(){
+
+});
